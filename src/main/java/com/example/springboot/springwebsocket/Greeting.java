93646648718e054e9c184a5c1e4c86dfaee2f5b6
Vikramsingh Kushwaha <130052532+V1kramK@users.noreply.github.com>
2023-05-05 15:53:03 +0530

    Add files via upload

---
+++ src/main/java/com/example/springboot/springwebsocket/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.springboot.springwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+    private String message;
+
+}
---
+++ src/main/java/com/example/springboot/springwebsocket/Scheduler.java
@@ -0,0 +1,19 @@
+package com.example.springboot.springwebsocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+@Configuration
+public class Scheduler {
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @Scheduled(fixedDelay = 5000)
+    public void sendMessage() {
+        template.convertAndSend("/topic/greetings",new Greeting("Scheduled Message"));
+    }
+}
